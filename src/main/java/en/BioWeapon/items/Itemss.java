bdd1865b217ba327ef084c9e139360e50e1cbc00
astalaD <82211520+astalaD@users.noreply.github.com>
2022-08-08 23:26:40 +0300

    Create Itemss.java

---
+++ src/main/java/en/BioWeapon/items/Itemss.java
@@ -0,0 +1,14 @@
+package en.BioWeapon.items;
+
+import en.BioWeapon.items.Item.ItemList;
+import net.fabricmc.api.ModInitializer;
+
+public class Itemss implements ModInitializer {
+
+    public static final String MOD_ID = "Itemss";
+
+    public void onInitialize() {
+        ItemList.init();
+    }
+
+}
