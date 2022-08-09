4e678cf6e6c09f18359d3c97a6d885ffbbac9dc4
astalaD <82211520+astalaD@users.noreply.github.com>
2022-08-09 15:57:05 +0300

    Create Biomass_ball.java

---
+++ src/main/java/en/BioWeapon/Bioweapontut/Item/Biomass_ball.java
@@ -0,0 +1,13 @@
+package en.BioWeapon.Bioweapontut.Item;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+
+public class Biomass_ball extends Item {
+
+    public Biomass_ball(Settings settings) {
+        super(settings);
+        settings.group(ItemGroup.MISC);
+    }
+
+}
